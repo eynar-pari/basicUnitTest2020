73ba5ed30f37de92d8aee6559a8ccb6407143dc0
eynar-pari <eynar_repc@hotmail.com>
2020-04-07 08:52:44 -0400

    ejercicio2

---
+++ src/main/java/ejercicio2/Ejercicio2.java
@@ -0,0 +1,13 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+
+    //  31 / enero / 2020
+    public static String nextDay(int day, String month, int year){
+
+        // todo
+        return "1 / febrero / 2020";
+    }
+
+
+}
